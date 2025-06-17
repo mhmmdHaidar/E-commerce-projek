55b7e91cd379ecaaf519715411910ac5c62912bb
mhmmdHaidar <mhmmdhaidar1212@gmail.com>
2025-06-17 20:53:43 +0700

    membuat file app untuk layout dan index untuk halaman home, sekligus setting home controller dan menambahkan folder asset yang berisi gambar ke dalam folder publik

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,11 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [HomeController::class, 'index'])->name('home.index');
