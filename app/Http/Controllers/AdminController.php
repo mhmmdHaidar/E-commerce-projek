14492b606cef21a61cfdc296c290cfe155576d2a
mhmmdHaidar <mhmmdhaidar1212@gmail.com>
2025-06-18 23:44:34 +0700

    membuat fitur add brands pada dashboard admin /07

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use App\Models\Brand;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Intervention\Image\Laravel\Facades\Image;
 
 class AdminController extends Controller
 {
@@ -14,7 +17,42 @@ class AdminController extends Controller
 
     public function brands()
     {
-        $brands = Brand::orderBy('id', 'desc')->paginate(10);
+        $brands = Brand::orderBy('created_at', 'desc')->paginate(10);
         return view('admin.brands', compact('brands'));
+    }
+
+    public function add_brand()
+    {
+        return view('admin.brand-add');
+    }
+
+    public function brand_store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'slug' => 'required|string|max:255|unique:brands,slug',
+            'image' => 'mimes:jpeg,png,jpg|max:2048',
+        ]);
+
+        $brand = new Brand();
+        $brand->name = $request->name;
+        $brand->slug = Str::slug($request->name);
+        $image = $request->file('image');
+        $file_extension = $request->file('image')->extension();
+        $file_name = Carbon::now()->timestamp . '.' . $file_extension;
+        $this->generatedBrandThumbnailsImage($image, $file_name);
+        $brand->image = $file_name;
+        $brand->save();
+        return redirect()->route('admin.brands')->with('status', 'Brand added successfully!');
+    }
+
+    public function generatedBrandThumbnailsImage($image, $imageName)
+    {
+        $destinationPath = public_path('uploads/brands');
+        $img = Image::read($image->path());
+        $img->cover(124, 124, "top");
+        $img->resize(124, 124, function ($constraint) {
+            $constraint->aspectRatio();
+        })->save($destinationPath . '/' . $imageName);
     }
 }
--- resources/views/admin/brands.blade.php
+++ resources/views/admin/brands.blade.php
@@ -32,10 +32,14 @@
                             </div>
                         </form>
                     </div>
-                    <a class="tf-button style-1 w208" href="add-brand.html"><i class="icon-plus"></i>Add new</a>
+                    <a class="tf-button style-1 w208" href="{{ route('admin.brand.add') }}"><i class="icon-plus"></i>Add
+                        new</a>
                 </div>
                 <div class="wg-table table-all-user">
                     <div class="table-responsive">
+                        @if (Session::has('status'))
+                            <p class="alert alert-success">{{ Session::get('status') }}</p>
+                        @endif
                         <table class="table table-striped table-bordered">
                             <thead>
                                 <tr>
@@ -49,7 +53,7 @@
                             <tbody>
                                 @foreach ($brands as $brand)
                                     <tr>
-                                        <td>{{ $brand->id }}</td>
+                                        <td>{{ $loop->iteration }}</td>
                                         <td class="pname">
                                             <div class="image">
                                                 <img src="{{ asset('uploads/brands') }}/{{ $brand->image }}"
