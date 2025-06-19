9b1bd742ce8da8e4ac11eaef03fe38027e24db20
mhmmdHaidar <mhmmdhaidar1212@gmail.com>
2025-06-19 21:38:52 +0700

    membuat halaman category pada dashboard admin /10

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Carbon\Carbon;
 use App\Models\Brand;
+use App\Models\Category;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\File;
@@ -99,5 +100,11 @@ class AdminController extends Controller
         }
         $brand->delete();
         return redirect()->route('admin.brands')->with('status', 'Brand has been deleted successfully!');
+    }
+
+    public function categories()
+    {
+        $categories = Category::orderBy('created_at', 'desc')->paginate(10);
+        return view('admin.categories', compact('categories'));
     }
 }
