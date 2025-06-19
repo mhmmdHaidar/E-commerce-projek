999b2ed3989c255d5d9741920bf33282ac9d38b0
mhmmdHaidar <mhmmdhaidar1212@gmail.com>
2025-06-19 21:17:17 +0700

    membuat fitur delete brand pada dashboard admin /09

--- resources/views/admin/brands.blade.php
+++ resources/views/admin/brands.blade.php
@@ -69,12 +69,15 @@
                                         <td><a href="#" target="_blank">1</a></td>
                                         <td>
                                             <div class="list-icon-function">
-                                                <a href="{{ route('admin.brand.edit', $brand->id) }}">
+                                                <a href="{{ route('admin.brand.edit', ['id' => $brand->id]) }}">
                                                     <div class="item edit">
                                                         <i class="icon-edit-3"></i>
                                                     </div>
                                                 </a>
-                                                <form action="#" method="POST">
+                                                <form action="{{ route('admin.brand.delete', ['id' => $brand->id]) }}"
+                                                    method="POST">
+                                                    @csrf
+                                                    @method('DELETE')
                                                     <div class="item text-danger delete">
                                                         <i class="icon-trash-2"></i>
                                                     </div>
@@ -95,3 +98,26 @@
         </div>
     </div>
 @endsection
+
+
+@push('scripts')
+    <script>
+        $(function() {
+            $('.delete').on('click', function(e) {
+                e.preventDefault();
+                var form = $(this).closest('form');
+                swal({
+                    title: "Are you sure?",
+                    text: "You want to delete this brand?",
+                    type: "warning",
+                    buttons: ["No", "Yes"],
+                    confirmButtonColor: "#dc3545",
+                }).then(function(result) {
+                    if (result) {
+                        form.submit();
+                    }
+                });
+            });
+        });
+    </script>
+@endpush
