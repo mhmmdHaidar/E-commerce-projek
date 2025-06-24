580811b55ced91ba4e897f98d0063c9e1f824b04
mhmmdHaidar <mhmmdhaidar1212@gmail.com>
2025-06-24 23:04:28 +0700

    merapihkan halaman index user pada bagian on sale / 48

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use App\Models\Product;
 use App\Models\Slide;
 use Illuminate\Http\Request;
 
@@ -12,7 +13,9 @@ class HomeController extends Controller
     {
         $slides = Slide::where('status', 1)->get()->take(3);
         $categories = Category::orderBy('name')->get();
-        return view('frontend.index', compact('slides', 'categories'));
+        $sproducts = Product::whereNotNull('sale_price')->where('sale_price', '<>', '')->inRandomOrder()->get()->take(8);
+
+        return view('frontend.index', compact('slides', 'categories', 'sproducts'));
     }
 
     public function about()
